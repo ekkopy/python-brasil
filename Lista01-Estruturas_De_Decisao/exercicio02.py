468eb17271033ba0f03109301f2db40b9313f193
ekkopy <thiagognlcn@gmail.com>
2020-02-29 15:20:35 -0300

    Exercício 02 - Positivo ou Negativo :scroll: :snake:

--- Lista01-Estruturas_De_Decisao/exercicio02.py
+++ Lista01-Estruturas_De_Decisao/exercicio02.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 """
- Exercício 02 - Faça um programa que peça um valor e mostre na tela se o valor é positivo
- ou negativo
+Exercício 02 - Faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo
 """
+
 
 number = float(input("Digite um numero: "))
 print("Positivo" if number > 0 else "Negativo")
