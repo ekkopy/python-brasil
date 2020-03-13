f7e02f8a87168a7c1eb78ed792e410e1a90e7b3c
ekkopy <thiagognlcn@gmail.com>
2020-03-13 17:26:05 -0300

    Exerício 01 - Nota vaĺida :scroll: :snake:

---
+++ Lista02_Estruturas_De_Repeticao/exercicio01.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*- 
+
+"""
+Faça um programa que peça uma nota, entre zero e dez. 
+Mostre uma mensagem caso o valor seja inválido e 
+continue pedindo até que o usuário informe um valor válido.
+""" 
+
+def ler_numero(nota = None):
+    nota = int(input("Digite uma nota entre zero e dez: "))
+    if nota >= 0 and nota <= 10:
+        print(f"Nota valida: {nota}")
+        return False # Quebra o loop
+        exit
+    else:
+        print(f"Nota invalida: {nota}\nPor favor informe uuma nota entre zero e dez!")
+        ler_numero(nota)
+
+while(ler_numero()):
+    ler_numero()
