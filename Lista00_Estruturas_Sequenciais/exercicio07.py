12cc8c9d3cf69d83aa60da806c90ce3c6eeb9c5a
ekkopy <thiagognlcn@gmail.com>
2020-03-18 23:11:20 -0300

    Exercício 07 - Área de um quadrado :scroll: :snake:

---
+++ Lista00_Estruturas_Sequenciais/exercicio07.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Faça um Programa que calcule a área de um quadrado, 
+em seguida mostre o dobro desta área para o usuário.
+"""
+
+lateral = float(input("Digite o valor das laterais: "))
+print(f"Valor da área: {lateral * lateral}\nDobro da área: {(lateral * lateral) * 2}")
