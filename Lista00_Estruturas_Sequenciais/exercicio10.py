d55e2d2858316896105e79ce1b94ba175776263c
ekkopy <thiagognlcn@gmail.com>
2020-03-18 23:43:58 -0300

    Exercicio 10 - Celsius para Farenheit :scroll: :snake:

---
+++ Lista00_Estruturas_Sequenciais/exercicio10.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Faça um Programa que peça a temperatura em graus Celsius, 
+transforme e mostre em graus Farenheit.
+"""
+
+celsius = float(input("Informe a temperatura em Celsius: "))
+print(f"{celsius}ºC equivalem a {(celsius * 9/5) + 32}ºF")
