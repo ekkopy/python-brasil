f74eba23d89c33cea5cd88773cf76fa9caf36ed6
ekkopy <thiagognlcn@gmail.com>
2020-03-18 21:52:00 -0300

    Exercício 03 - Soma de números :scroll: :snake:

---
+++ Lista00_Estruturas_Sequenciais/exercicio03.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+Faça um Programa que peça dois números e imprima a soma.
+"""
+n1 = float(input("Digite o primeiro numero: "))
+n2 = float(input("Digite o segundo numero: "))
+print(f"{n1} + {n2} = {n1 + n2}")
