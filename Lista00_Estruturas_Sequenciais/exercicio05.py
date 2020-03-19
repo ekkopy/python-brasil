012c5cd434580fddd7c5e4379056c449ad1cb26f
ekkopy <thiagognlcn@gmail.com>
2020-03-18 22:38:51 -0300

    Exercício 05 - Metros para Centimentros :scroll: :snake:

---
+++ Lista00_Estruturas_Sequenciais/exercicio05.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+Faça um Programa que converta metros para centímetros.
+"""
+
+metros = float(input("Digite o valor em metros: "))
+print(f"O valor de {metros} em cm => {metros * 100} cm")
