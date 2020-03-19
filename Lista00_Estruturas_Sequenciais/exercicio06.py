27d958b45d69a8013d4a61304a1a3c4b124b2b34
ekkopy <thiagognlcn@gmail.com>
2020-03-18 22:57:27 -0300

    Exercício 06 - Calculo de raio :scroll: :snake:

---
+++ Lista00_Estruturas_Sequenciais/exercicio06.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8  -*-
+
+"""
+Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+"""
+
+from math import pi
+
+
+raio = int(input("Informe a medida do raio: "))
+print(f"Area do circulo do raio: {raio} e aproximadamente: {(raio * raio) * pi} mm²")
