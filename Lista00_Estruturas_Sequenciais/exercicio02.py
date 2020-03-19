830f406a94ca7ffdb218dd35f335d41edcee84b4
ekkopy <thiagognlcn@gmail.com>
2020-03-18 21:46:53 -0300

    Exercício 02 - Qual o número ? :scroll: :snake:

---
+++ Lista00_Estruturas_Sequenciais/exercicio02.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+Faça um Programa que peça um número e 
+então mostre a mensagem O número informado foi [número].
+"""
+
+print(f"O numero iinformado foi: {float(input('Digite um numero: '))}")
