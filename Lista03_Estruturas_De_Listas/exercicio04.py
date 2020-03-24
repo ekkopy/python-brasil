805fe9781e1ac93ff298a74b00a4136afd0f8fd8
ekkopy <thiagognlcn@gmail.com>
2020-03-24 20:44:42 -0300

    Exercício 04 - Consoantes :scroll: :snake:

---
+++ Lista03_Estruturas_De_Listas/exercicio04.py
@@ -0,0 +1,21 @@
+# -*- coding> utf-8 -*-
+
+# Faça um Programa que leia um vetor de 10 caracterese diga quantas consoantes foram lidas. Imprima as consoantes.
+
+consoantes = []
+
+vogais = ['a', 'e', 'i', 'o', 'u']
+
+novasConsoantes =  []
+
+for letra in range(0, 10):
+    consoantes.append(input("Digite o " + str(letra) + " caractere: ").lower())
+
+totalConsoantes = 0
+
+for x in range(0, 10):
+    if not consoantes[x] in vogais:
+        novasConsoantes.append(consoantes[x])
+        totalConsoantes += 1
+
+print(f"Total de consoantes: {totalConsoantes}\nConsoantes: {novasConsoantes}")
