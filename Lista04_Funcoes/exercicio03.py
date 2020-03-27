5f94104e35923efe3d7f20d65789cdbfd01947fa
ekkopy <thiagognlcn@gmail.com>
2020-03-27 19:57:20 -0300

    Exercício 03 - Soma de Argumentos :scroll: :snake:

---
+++ Lista04_Funcoes/exercicio03.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+def somarArgs(args = []) -> int:
+   count = 0
+   for x in args:
+       count += x
+   return count
+
+numeros = []
+for x in range(1, 4):
+    numeros.append(int(
+        input("Digite o " + str(x) + " numero: ")))
+
+print(f"Somatório: {somarArgs(numeros)}")
+
