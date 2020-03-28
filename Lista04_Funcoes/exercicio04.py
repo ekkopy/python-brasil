dfbe5b71903df144ca32a151653ab797e1c1a5c3
ekkopy <thiagognlcn@gmail.com>
2020-03-27 21:51:25 -0300

    Exercício 04 - Argumento positivo ou negativo :scroll: :snake:

---
+++ Lista04_Funcoes/exercicio04.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+def returnElement(number) -> str:
+    if number > 0:
+        return 'P'
+    return 'N'
+
+
+print(returnElement(
+    int(input("Digite um numero: "))
+    )
+)
