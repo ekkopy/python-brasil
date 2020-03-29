0a9d34b1903de2e0a96d2af16ee4d870fca93978
ekkopy <thiagognlcn@gmail.com>
2020-03-29 15:44:36 -0300

    Exercício 05 - Taxa de imposto :scroll: :snake:

--- Lista04_Funcoes/exercicio05.py
+++ Lista04_Funcoes/exercicio05.py
@@ -4,7 +4,6 @@ def somaImposto(taxaImposto, custo) -> float:
     custo = custo + (custo * taxaImposto / 100)
     return custo
 
-
 taxaImposto = float(input("Informe o valor da taxa de imposto: "))
 custo = float(input("Informe o valor do produto: "))
 
