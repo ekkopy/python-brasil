46ddcf58431b221656c277fa508aedb511d9bb1c
ekkopy <thiagognlcn@gmail.com>
2020-04-09 13:22:14 -0300

    Exercício 04 - Taxa de crescimento :scroll: :snake:

--- Lista02_Estruturas_De_Repeticao/exercicio04.py
+++ Lista02_Estruturas_De_Repeticao/exercicio04.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 
+
+"""
+Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% e 
+que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. Faça um programa que 
+calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, 
+mantidas as taxas de crescimento.
+"""
+
 popA = 80000
 crescA = 1.03
 popB = 200000
