6752b6f9e66cbcfc4549b5d13925ebd1e564f457
ekkopy <thiagognlcn@gmail.com>
2020-04-12 20:58:52 -0300

    Exercício 03 - Soma de argumentos :scroll: :snake:

--- Lista04_Funcoes/exercicio03.py
+++ Lista04_Funcoes/exercicio03.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+"""
+Faça um programa, com uma função que necessite de três argumentos, 
+e que forneça a soma desses três argumentos.
+"""
+
 def somarArgs(args = []) -> int:
    count = 0
    for x in args:
@@ -7,6 +12,7 @@ def somarArgs(args = []) -> int:
    return count
 
 numeros = []
+
 for x in range(1, 4):
     numeros.append(int(
         input("Digite o " + str(x) + " numero: ")))
