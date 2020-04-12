b511b7e99d7279c059844fd62b2c7370dcf091ff
ekkopy <thiagognlcn@gmail.com>
2020-04-12 20:56:56 -0300

    Exercício 02 - Repetir até N numero :scroll: :snake:

--- Lista04_Funcoes/exercicio02.py
+++ Lista04_Funcoes/exercicio02.py
@@ -1,10 +1,27 @@
 # -*- coding: utf-8 -*-
 
-def repeatTillN(numbr):
-   for x in range(1, numbr + 1):
-    for y in range(x, x+1):
-        print(f"{y} ", end='')  # com o end você quebra o \n
-    
-repeatTillN(
-    int(input("Digite um numero: "))
+"""
+Faça um programa para imprimir:
+    1
+    1   2
+    1   2   3
+    .....
+    1   2   3   ...  n
+para um n informado pelo usuário. Use uma função que receba um valor n inteiro imprima até a n-ésima linha.
+"""
+
+def repetirAteN(numero):
+    x = 1
+    if isinstance(numero, int):
+       while x <= numero:
+            y = 1
+            separador = ''
+            while y <= x:
+               separador += f"{str(y)}\t"
+               y+=1
+            print(separador)
+            x+=1
+
+repetirAteN(
+    int(input("Informe um numero: "))
 )
