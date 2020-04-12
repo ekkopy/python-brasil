4ce239346bcf7c9480887b2c23c4188b35b5782e
ekkopy <thiagognlcn@gmail.com>
2020-04-12 20:27:35 -0300

    Exercício 05 - Vetor par ou ímpar :scroll: :snake:

--- Lista03_Estruturas_De_Listas/exercicio05.py
+++ Lista03_Estruturas_De_Listas/exercicio05.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 """
 Faça um Programa que leia 20 números inteiros e armazene-os num vetor. 
 Armazene os números pares no vetor PAR e os números IMPARES no vetor impar.
