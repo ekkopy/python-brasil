ea734d1279929fe321c8ebb3da720e8fa0310401
ekkopy <thiagognlcn@gmail.com>
2020-04-12 21:01:46 -0300

    Exercício 05 - Somatório de Imposto :scroll: :snake:

--- Lista04_Funcoes/exercicio05.py
+++ Lista04_Funcoes/exercicio05.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+
+"""
+Faça um programa com uma função chamada somaImposto. A função possui dois parâmetros formais: taxaImposto, que é a quantia de imposto sobre vendas expressa em porcentagem e custo, que é o custo de um item antes do imposto. 
+A função “altera” o valor de custo para incluir o imposto sobre vendas.
+"""
+
 def somaImposto(taxaImposto, custo) -> float:
     custo = custo + (custo * taxaImposto / 100)
     return custo
